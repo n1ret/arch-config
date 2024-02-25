f8c9a22e6014b1b6905b79f2c17fa80a7b7a6256
n1ret <ytolegonik@gmail.com>
2024-02-25 21:32:43 +0500

    default toolchain selecting added

--- install_paru.sh
+++ install_paru.sh
@@ -9,6 +9,8 @@ if ! command -v rustup > /dev/null; then
     ./install_rust.sh
 fi
 
+[ $(rustup show active-toolchain | wc -l) -ne 0 ] || rustup default stable
+
 rm -rf paru
 git clone https://aur.archlinux.org/paru.git
 cd paru
