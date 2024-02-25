2b3d8e78d63f59c254749410760ef3e14cb1d730
n1ret <n1ret.lcode@gmail.com>
2024-02-26 00:06:25 +0500

    paru installation fix

--- install_paru.sh
+++ install_paru.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 start=$PWD
+arch_config=$(dirname $(realpath $0))
 cd /tmp
 
 sudo pacman -S --needed base-devel
 
 if ! command -v rustup > /dev/null; then 
-    ./install_rust.sh
+    $arch_config/install_rust.sh
 fi
 
 [ $(rustup show active-toolchain | wc -l) -ne 0 ] || rustup default stable
