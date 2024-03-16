585d71ffba57004e3336243f63b842a9786cdd6b
n1ret <n1ret.lcode@gmail.com>
2024-03-16 12:03:38 +0500

    logout gracefulled

--- configs/hyprland/execute.sh
+++ configs/hyprland/execute.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo pacman -S hyprland hyprpaper sddm \
+               jq \
                qt5-quickcontrols2 qt5-graphicaleffects qt5-svg \
                pipewire pipewire-pulse \
                firefox
