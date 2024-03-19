add8d669980595e8915c6361d7aef9f205495868
n1ret <n1ret.lcode@gmail.com>
2024-03-19 19:13:25 +0500

    polkit added

--- configs/hyprland/execute.sh
+++ configs/hyprland/execute.sh
@@ -2,6 +2,8 @@
 
 sudo pacman -S hyprland hyprpaper sddm \
                jq \
+               polkit-kde-agent \
+               xdg-desktop-portal-hyprland \
                qt5-quickcontrols2 qt5-graphicaleffects qt5-svg \
                pipewire pipewire-pulse \
                firefox
