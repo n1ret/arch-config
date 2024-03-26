62190f002f7408ec19de07bbf4ce93e725cb14dd
n1ret <n1ret.lcode@gmail.com>
2024-03-26 20:41:33 +0500

    cliphist added

--- configs/hyprland/execute.sh
+++ configs/hyprland/execute.sh
@@ -2,12 +2,13 @@
 
 sudo pacman -S hyprland hyprpaper hyprlock hypridle \
                jq \
+               wofi \
                brightnessctl \
                thunar \
                polkit-kde-agent \
                xdg-desktop-portal-hyprland \
                sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg \
                pipewire pipewire-pulse \
-               wl-clipboard \
+               wl-clipboard cliphist \
                firefox
 
