b1df52e2754146b1b709b567e113547760a881a2
n1ret <n1ret.lcode@gmail.com>
2024-05-04 13:05:17 +0500

    mode fixes

--- configs/hyprland/execute.sh
+++ configs/hyprland/execute.sh
@@ -4,20 +4,22 @@ base_dir="$(dirname $(realpath "$0"))/../.."
 
 $base_dir/scripts/install_paru
 
-paru -S hyprland hyprpaper hyprlock hypridle \
-        jq \
-        rofi-lbonn-wayland wtype \
-        brightnessctl \
-        thunar \
-        polkit-kde-agent \
-        xdg-desktop-portal-hyprland \
-        sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg \
-        pipewire pipewire-pulse \
-        wl-clipboard cliphist \
-        waybar \
-        mako \
-        neovim \
-        firefox
+paru --needed -S \
+    hyprland hyprpaper hyprlock hypridle \
+    jq \
+    rofi-lbonn-wayland wtype \
+    brightnessctl \
+    thunar \
+    polkit-kde-agent \
+    xdg-desktop-portal-hyprland \
+    sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg \
+    pipewire pipewire-pulse \
+    wl-clipboard cliphist \
+    waybar \
+    mako \
+    neovim \
+    firefox
 
 sudo systemctl enable sddm.service
 systemctl enable --user pipewire-pulse.service pipewire.service
+
