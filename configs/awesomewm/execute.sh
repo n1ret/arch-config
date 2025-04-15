d15a0149609ad63deefa425ce20ee81c947274d0
N1ret <n1ret.lcode@gmail.com>
2025-04-15 22:16:05 +0500

    awesome numlock

--- configs/awesomewm/execute.sh
+++ configs/awesomewm/execute.sh
@@ -5,7 +5,7 @@ base_dir="$(dirname $(realpath "$0"))/../.."
 $base_dir/scripts/install_paru
 
 paru --needed -S \
-    awesome \
+    awesome numlockx \
     lightdm lightdm-gtk-greeter \
     kitty
 
