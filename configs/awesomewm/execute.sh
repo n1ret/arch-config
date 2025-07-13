f9bfe331136b417077877680c1dad356de44d69b
N1ret <n1ret.lcode@gmail.com>
2025-07-13 20:45:42 +0500

    aweSOMEwm changes

--- configs/awesomewm/execute.sh
+++ configs/awesomewm/execute.sh
@@ -5,7 +5,7 @@ base_dir="$(dirname $(realpath "$0"))/../.."
 $base_dir/scripts/install_paru
 
 paru --needed -S \
-    awesome numlockx \
-    lightdm lightdm-gtk-greeter \
+    awesome numlockx gtk-engine-murrine \
+    sddm sddm-silent-theme \
     kitty
 
