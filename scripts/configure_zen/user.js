e165c3c1db4365c0c9b1bfaf7eff7a26ee40fa62
n1ret <n1ret.lcode@gmail.com>
2025-09-15 00:43:28 +0300

    zen user.js installation script added

---
+++ scripts/configure_zen/user.js
@@ -0,0 +1,7 @@
+// toggle off the overscroll
+user_pref("apz.overscroll.enabled", false);
+
+// toggle off horizontal swipes
+user_pref("browser.gesture.swipe.left", "");
+user_pref("browser.gesture.swipe.right", "");
+
