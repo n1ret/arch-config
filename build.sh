a359fec54fa2d7cc2752a3f12cdfea0a03686181
n1ret <n1ret.lcode@gmail.com>
2026-03-25 15:57:08 +0300

    Building isolation

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/sh
 
+prev_dir="$(pwd)"
 script_dir="$(dirname "$(realpath "$0")")"
 
-mkdir -p "$script_dir/bin" && \
-  python -m venv __venv && \
-  __venv/bin/pip install pyinstaller && \
-  __venv/bin/pyinstaller --onefile --distpath "$script_dir/bin" arch-cfg.py && \
-  sudo rm -rf __venv build __pycache__ arch-cfg.spec
+mkdir -p "$script_dir/bin" &&
+  cd "$script_dir/bin" && {
+  python -m venv __venv &&
+    __venv/bin/pip install pyinstaller &&
+    __venv/bin/pyinstaller --onefile --distpath "$script_dir/bin" "$script_dir/arch-cfg.py"
+
+  sudo rm -rf __venv build arch-cfg.spec
+}
+
+cd "$prev_dir" || exit
